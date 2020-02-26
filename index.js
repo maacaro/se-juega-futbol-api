fdd943c2a700b91786e7f5aaad5c37476ca3aab1
Manuel Castro <maacaro@gmail.com>
2020-02-26 09:11:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3005;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js Express and Postgres" });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
