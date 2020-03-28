af4f75bcfca364999fa6ea2fe6d28ab5811a10dd
Manuel Castro <maacaro@gmail.com>
2020-03-28 19:55:08 -0400

    SJF-006 logs errors

--- src/api/user/controllers.js
+++ src/api/user/controllers.js
@@ -15,6 +15,7 @@ async function postUser(req, res) {
 
     return res.status(201).send({ ...response });
   } catch (error) {
+    console.log(error);
     return res.status(500).send(error);
   }
 }
